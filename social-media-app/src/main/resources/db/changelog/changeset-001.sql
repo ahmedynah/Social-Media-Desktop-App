6f3ea20dde80589c552615ad563d351fec8dd9ae
Muhammad Zayed <muhammadzayed810@gmail.com>
2025-01-15 22:43:36 +0200

    feat(Authentication):modify users table fields

--- social-media-app/src/main/resources/db/changelog/changeset-001.sql
+++ social-media-app/src/main/resources/db/changelog/changeset-001.sql
@@ -1,7 +1,8 @@
 -- Creating the USERS table with an ID, USERNAME, and PASSWORD columns
-CREATE TABLE USERS (
-    ID BIGINT AUTO_INCREMENT PRIMARY KEY,  -- The primary key for the table, an auto-incrementing BIGINT
-    USERNAME VARCHAR(255) NOT NULL,         -- A column for storing the username, it cannot be null
-    PASSWORD VARCHAR(1024) NOT NULL         -- A column for storing the password, it cannot be null
+CREATE TABLE users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,  -- The primary key for the table, an auto-incrementing BIGINT
+    first_name VARCHAR(255) NOT NULL,         -- A column for storing the username, it cannot be null
+    last_name VARCHAR(255) NOT NULL,         -- A column for storing the username, it cannot be null
+    email VARCHAR(255) NOT NULL,         -- A column for storing the username, it cannot be null
+    password VARCHAR(1024) NOT NULL         -- A column for storing the password, it cannot be null
 );
-
