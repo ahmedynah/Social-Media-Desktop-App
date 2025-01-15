13aa1d235e8e52a93a0d562f0402a88a48acf5fd
Muhammad Zayed <muhammadzayed810@gmail.com>
2025-01-15 16:22:00 +0200

    feat(Authentication): create login and register pages

--- social-media-app/src/main/resources/db/changelog/changeset-001.sql
+++ social-media-app/src/main/resources/db/changelog/changeset-001.sql
@@ -5,12 +5,3 @@ CREATE TABLE USERS (
     PASSWORD VARCHAR(1024) NOT NULL         -- A column for storing the password, it cannot be null
 );
 
--- Adding comments to the table and columns using ALTER TABLE
-ALTER TABLE USERS COMMENT = 'Table storing user information such as ID, username, and password';
-
--- Adding comments to individual columns
-ALTER TABLE USERS MODIFY COLUMN ID BIGINT AUTO_INCREMENT COMMENT 'The primary key, auto-incremented';
-ALTER TABLE USERS MODIFY COLUMN USERNAME VARCHAR(255) NOT NULL COMMENT 'The username for the user, cannot be null';
-ALTER TABLE USERS MODIFY COLUMN PASSWORD VARCHAR(1024) NOT NULL COMMENT 'The password for the user, cannot be null';
-
--- End of changeset
